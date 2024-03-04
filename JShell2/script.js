f8fbc8dd44ed301643142cb2311eafd6035b70c4
bhaviljain <bhaviljain26@gmail.com>
2024-03-04 21:32:32 +0530

    swap the image

---
+++ JShell2/script.js
@@ -0,0 +1,11 @@
+let one = document.getElementById('one');
+let two = document.getElementById('two');
+let btn = document.getElementById('btn');
+
+btn.addEventListener('click', ()=>{
+    let img1 = one.src
+    let img2 = two.src
+
+    one.src = img2
+    two.src = img1
+})
