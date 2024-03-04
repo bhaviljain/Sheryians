5f8ea5e37c9c51128629401ee396dd736c87d444
bhaviljain <bhaviljain26@gmail.com>
2024-03-04 21:33:18 +0530

    change the text

---
+++ JShell1/script.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById('btn')
+let p = document.querySelector('p')
+
+btn.addEventListener('click', function(){
+//   p.innerText = 'Hello World!'
+  p.textContent = 'Hello World!'
+})
